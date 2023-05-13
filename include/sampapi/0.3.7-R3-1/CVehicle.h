b587b292595ca45c57f425a9b48fbb9ce66d01d6
Northn <erolerol3760@gmail.com>
2023-05-13 20:02:49 +0400

    Fix CVehicle fields positions

--- include/sampapi/0.3.7-R3-1/CVehicle.h
+++ include/sampapi/0.3.7-R3-1/CVehicle.h
@@ -25,14 +25,15 @@ public:
     CVehicle*          m_pTrailer;
     ::CVehicle*        m_pGameVehicle;
     BOOL	       m_bEngineOn;
-    BOOL               m_bIsInvulnerable;
     BOOL               m_bIsLightsOn;
+    BOOL               m_bIsInvulnerable;
+    int                pad_5c;
     BOOL               m_bIsLocked;
     bool               m_bIsObjective;
     BOOL               m_bObjectiveBlipCreated;
     TICK               m_timeSinceLastDriven;
     BOOL               m_bHasBeenDriven;
-    char               pad_71[4];
+    int                pad_71;
     BOOL               m_bEngineState;
     unsigned char      m_nPrimaryColor;
     unsigned char      m_nSecondaryColor;
@@ -40,7 +41,7 @@ public:
     BOOL               m_bUnoccupiedSync;
     BOOL               m_bRemoteUnocSync;
     BOOL               m_bKeepModelLoaded;
-    int                m_bHasSiren;
+    BOOL               m_bHasSiren;
     IDirect3DTexture9* m_pLicensePlate;
     char               m_szLicensePlateText[MAX_LICENSE_PLATE_TEXT + 1];
     GTAREF             m_marker;
